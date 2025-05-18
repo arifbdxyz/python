5acf117c2c79c322c25e38ca88a341757add1441
Md Ariful Islam <arifbdxyz@gmail.com>
2025-05-18 14:56:03 +0600

    Create comment.py
    syntax comment

---
+++ comment.py
@@ -0,0 +1,12 @@
+# this is my comment
+print("I Love Python")
+
+
+print("I Love Programing") # this my an comment
+
+"""
+for multiple lines comments 
+My Name is Arif
+I am a Programer
+"""
+print("Welcome Everyone") 
